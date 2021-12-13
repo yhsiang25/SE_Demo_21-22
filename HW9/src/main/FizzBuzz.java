bf78762cd8a336c2048c60d8e9db2aaa3a9458af
yhsiang25 <yhsiang.chen@outlook.com>
2021-12-13 02:16:26 +0100

    task 3, method completed

--- HW9/src/main/FizzBuzz.java
+++ HW9/src/main/FizzBuzz.java
@@ -3,7 +3,19 @@ package main;
 public class FizzBuzz {
 	
 	public String convertNumber(int number) {
-		// Implement
-		return "";
+		//5, 25, 50, 55, 75, 100 (all % 5 == 0)
+		if (number % 5 == 0 && number % 7 != 0) {
+			return "Fizz";
+		}
+		//7, 49, 84, 91, 98, 28 (all % 7 == 0)
+		else if (number % 5 != 0 && number % 7 == 0) {
+			return "Buzz";
+		}
+		//35, 140, 280, 1120, 70, 105 (%5 AND %7 == 0)
+		else if (number % 5 == 0 && number % 7 == 0) {
+			return "FizzBuzz";
+		}
+		
+		else return "Booom... no match";
 	}
 }
